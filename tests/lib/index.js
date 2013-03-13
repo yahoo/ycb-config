e0afb0b7ca00ba383a9092fd2d4877553553f4a1
Drew Folta <drew@folta.net>
2013-03-13 14:30:39 -0700

    setting up testing

---
+++ tests/lib/index.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2013, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE.txt file for terms.
+ */
+
+
+/*jslint nomen:true, white:true, node:true */
+/*globals describe, it */
+"use strict";
+
+
+var libpath = require('path'),
+    expect = require('chai').expect,
+    Config = require('../../lib/index');
+
+
+describe('config', function() {
+
+
+});
+
+
