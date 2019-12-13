27e578da75dceabb1cdfaddfcbd073b8cae97097
Seth Bertalotto <seth@bertalotto.net>
2019-12-13 07:50:03 -0800

    Revert "fix: revert time aware feature (#20)" (#23)

---
+++ tests/fixtures/touchdown-simple/configs/undefined-config.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2013 Yahoo! Inc. All rights reserved.
+ * Copyrights licensed under the BSD License.
+ * See the accompanying LICENSE.txt file for terms.
+ */
+
+
+module.exports = [
+    {
+        settings: [ 'master' ],
+        TODO: 'TODO'
+    },
+    {
+        settings: [ 'device:mobile' ],
+        selector: 'mobile',
+        foo: undefined
+    }
+];
