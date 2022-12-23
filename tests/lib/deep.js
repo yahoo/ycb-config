d613b76039c0192cf13497f80e330f97f6893379
dazedbear <angus912584@gmail.com>
2022-12-23 18:56:03 +0800

    test: rewrite unit test for deepFreeze

--- tests/lib/deep.js
+++ tests/lib/deep.js
@@ -1,13 +1,27 @@
-var test = require('tap').test;
-var deepFreeze = require('../');
+'use strict';
 
-test('deep freeze', function (t) {
-    t.plan(2);
-    
-    deepFreeze(Buffer);
-    Buffer.x = 5;
-    t.equal(Buffer.x, undefined);
-    
-    Buffer.prototype.z = 3;
-    t.equal(Buffer.prototype.z, undefined);
+var expect = require('chai').expect,
+    deepFreeze = require('../../lib/deep');
+
+function assertImmutableObject (object, modifier) {
+    var catchedErr = null;
+    try {
+        modifier(object);
+    } catch (err) {
+        catchedErr = err;
+    }
+    expect(catchedErr).to.be.an['instanceof'](Error);
+    expect(catchedErr.message).to.include('object is not extensible');
+}
+
+describe('deep freeze', function () {
+    it('should be immutable', function () {
+        deepFreeze(Buffer);
+        assertImmutableObject(Buffer, function (obj) {
+            obj.x = 5;
+        });
+        assertImmutableObject(Buffer, function (obj) {
+            obj.prototype.z = 3;
+        });
+    });
 });
